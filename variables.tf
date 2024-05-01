d92a89a9382b38dca4b89a6d13a6bc9b676635a8
divya <divya@gmail.com>
2024-05-01 18:45:37 +0530

    tf files

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable environment{
+    default = "DevTesting"
+}
+
+variable resource_group{
+    default= "DevEnv"
+}
+
+variable location{
+    default = "East Asia"
+}
+
+variable node_count{
+    default = 2
+}
+
+variable cluster_name{
+    default = "Devk8sCluster"
+}
+
+variable dns_prefix{
+    default = "devtest"
+}
+
+variable ssh_public_key {
+    
+}
+
+variable client_id {
+    
+}
+variable client_secret {
+    
+}
